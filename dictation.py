689f5220ea59d235d133dd725aaaadd55e247e7b
Ben Thomasson <bthomass@redhat.com>
2023-01-09 09:41:49 -0500

    Add a background thread for recognizing the audio

--- dictation.py
+++ dictation.py
@@ -12,63 +12,76 @@ Options:
 from docopt import docopt
 import logging
 import sys
-import os
 import speech_recognition as sr
-import pyaudio
 import time
-import subprocess
-import string
+from threading import Thread
+from queue import Queue
 
-logger = logging.getLogger('computer')
+logger = logging.getLogger("computer")
+
+
+# background recognizer thread
+def recognize_audio_thread(r, queue):
+    while True:
+        audio_data = queue.get()
+        try:
+            print("Recognizing...", file=sys.stderr)
+            start = time.time()
+            text = r.recognize_whisper(audio_data)
+            print("Took ", time.time() - start, file=sys.stderr)
+        except KeyboardInterrupt:
+            return
+        text = text.strip()
+        if text == "":
+            continue
+
+        print(text)
 
 
 def parse_args(args):
     parsed_args = docopt(__doc__, args)
-    if parsed_args['--debug']:
+    if parsed_args["--debug"]:
         logging.basicConfig(level=logging.DEBUG)
-    elif parsed_args['--verbose']:
+    elif parsed_args["--verbose"]:
         logging.basicConfig(level=logging.INFO)
     else:
         logging.basicConfig(level=logging.WARNING)
     return parsed_args
 
 
-def recognize_audio(r, source):
+def listen_for_audio(r, source, queue):
     # read the audio data from the default microphone
-    try:
-        audio_data = r.listen(source, timeout=5)
-        # play(audio_data)
-        # convert speech to text
-        text = r.recognize_whisper(audio_data)
-    except sr.WaitTimeoutError:
-        text = ""
-    return text
+    while True:
+        try:
+            print("Listening...", file=sys.stderr)
+            audio_data = r.listen(source, timeout=5)
+            queue.put(audio_data)
+        except sr.WaitTimeoutError:
+            pass
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    parsed_args = parse_args(args)
+    parse_args(args)
 
     r = sr.Recognizer()
     with sr.Microphone(sample_rate=8000) as source:
         r.adjust_for_ambient_noise(source, duration=5)
         print("Ready", file=sys.stderr)
-        while True:
-            try:
-                text = recognize_audio(r, source)
-            except KeyboardInterrupt:
-                return
-            text = text.strip()
-            if text == "":
-                continue
+        audio_queue = Queue()
+        listener_thread = Thread(target=listen_for_audio,
+                                 args=(r, source, audio_queue))
+        listener_thread.start()
+        recognizer_thread = Thread(target=recognize_audio_thread,
+                                   args=(r, audio_queue))
+        recognizer_thread.start()
 
-            print(text)
-            # generate_response(text)
-            time.sleep(1)
+        listener_thread.join()
+        recognizer_thread.join()
 
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
